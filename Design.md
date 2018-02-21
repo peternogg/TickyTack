4a003208e7fd07bbfc1bc297f8eacfa6922691e3
Peter Higginbotham <peternogg@gmail.com>
2018-02-20 23:11:38 -0800

    Added CURSOR-y mouse support
    The program tracks the mouse's movements and makes the screen cursor
    follow its position. Also, (sometimes) when you click, a c appears where
    the cursor was. It's inconsistent, maybe cuz of ncurses or the terminal.

--- Design.md
+++ Design.md
@@ -1,3 +1,8 @@
 TickyTack will be written around a regular game loop, except the update method will pause until it gets user input via the mouse cursor or keyboard. 
 
 The board is represented as a 9 unit char array, which is redrawn every frame
+
+## Screen Layout
+See "ScreenLayout.txt" for an example.
+
+The console is divided into two parts: A play area, and a log area. The play area will take up most of the screen and resides on the upper half. The log uses the remaining amount on the bottom. The screen should be split into thirds, and the play area takes the upper two, while the log takes the bottom one.
--- source/TickyTack.c
+++ source/TickyTack.c
@@ -10,21 +10,32 @@ int main() {
     // Prep the screen
     initscr();
     // Poll inputs every 1/10th of a second
-    halfdelay(1);
+    cbreak();
     // Don't echo anything to the screen
     noecho();
+    keypad(stdscr, true);
+
+    // Ask for mouse position info
+    mousemask(REPORT_MOUSE_POSITION | BUTTON1_CLICKED | BUTTON1_PRESSED, NULL);
 
     Game_t game;
+    MEVENT mouseEvent;
 
     Game_init(&game);
 
     while(game.keepPlaying) {
-        char c = getch();
+        int c = getch();
 
-        if (c != ERR)
-            mvprintw(0, 0, "You typed %c\n", c);
-        else
-            mvprintw(0, 0, "Ah, no input?");
+        if (c == KEY_MOUSE) {
+            // Whoa! Mouse event!
+            if (getmouse(&mouseEvent) == OK) {
+                if (mouseEvent.bstate & REPORT_MOUSE_POSITION)
+                    move(mouseEvent.y, mouseEvent.x);
+                
+                if (mouseEvent.bstate & BUTTON1_CLICKED || mouseEvent.bstate & BUTTON1_PRESSED)
+                    addch('c');
+            }
+        }
 
         if (c == 'q')
             game.keepPlaying = false;
@@ -33,10 +44,4 @@ int main() {
     }
 
     endwin();
-}
-
-void DrawBoard(Board_t* board) {
-    mvprintw(0, 0, "%c | %c | %c\n", board->spaces[0], board->spaces[1], board->spaces[2]);
-    printw("%c | %c | %c\n", board->spaces[3], board->spaces[4], board->spaces[5]);
-    printw("%c | %c | %c\n", board->spaces[6], board->spaces[7], board->spaces[8]);
 }
